43cd8a30fd4fe7c5579630c4e0592dc8fc5564ae
mukotso <kelvinmukotso@gmail.com>
2022-02-27 18:45:15 +0300

    changes before merge

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/F-Project/cytonn_register/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/F-Project/cytonn_register/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kmukotso/Desktop/projects/F-PROJECT/cytonn_register/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kmukotso/Desktop/projects/F-PROJECT/cytonn_register/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

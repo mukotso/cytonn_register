1cc00775bdf66d14ba0ef6fde9867c960d545956
mukotso <kelvinmukotso@gmail.com>
2022-03-01 17:23:29 +0300

    fix custom validation error to match formRequest validation errors

--- app/Repositories/userProfileRepository.php
+++ app/Repositories/userProfileRepository.php
@@ -16,17 +16,17 @@ class userProfileRepository implements UserProfileRepositoryInterface
 
     public function updatePassword($request)
     {
-        $hashed_password=auth()->user()->password;
-        $oldPassword=$request->oldpassword;
-        $newPassword=$request->password;
+        $hashed_password = auth()->user()->password;
+        $oldPassword = $request->oldpassword;
+        $newPassword = $request->password;
         if (Hash::check($oldPassword, $hashed_password)) {
             if (!Hash::check($newPassword, $hashed_password)) {
                 $id = auth()->user()->id;
-                $user = User::where('id',$id)->first();
-                $user->password=Hash::make($newPassword);
+                $user = User::where('id', $id)->first();
+                $user->password = Hash::make($newPassword);
                 $user->save();
 
-                return response()->json(['message' => "Password updated sucessfully"],200);
+                return response()->json(['message' => "Password updated sucessfully"], 200);
             } else {
                 return response()->json([
                     'errors' => json_decode('{ "password": "Old Password can be same as new password please try setting a different password" }'),
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
 
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"  />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
